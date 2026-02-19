af22bcd5624a9fba881ca54c93066785a980571b
Thilo <thilo0123@gmail.com>
2026-02-19 02:14:14 +0100

    getFullUser returns groups now

--- Sources/FynnCloudBackend/Extensions/Request+user.swift
+++ Sources/FynnCloudBackend/Extensions/Request+user.swift
@@ -5,7 +5,12 @@ extension Request {
     func getFullUser() async throws -> User {
         let payload = try auth.require(UserPayload.self)
         let userID = try payload.getID()
-        guard let user = try await User.find(userID, on: db) else {
+        guard
+            let user = try await User.query(on: db)
+                .filter(\.$id == userID)
+                .with(\.$groups)
+                .first()
+        else {
             throw Abort(.notFound, reason: "User not found").localized("error.unauthorized")
         }
         return user
