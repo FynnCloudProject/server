6806d973c46630fa7c71caed55cd922269ebba2f
Thilo <thilo0123@gmail.com>
2026-09-07 15:20:29 +0200

    Remove service injection/move to the single file

--- Sources/FynnCloudServer/Lifecycle/LDAPLifecycleHandler.swift
+++
@@ -1,8 +0,0 @@
-import Vapor
-
-struct LDAPLifecycleHandler: LifecycleHandler {
-    func shutdownAsync(_ application: Application) async {
-        await application.services.ldap.service.disconnect()
-        application.logger.info("LDAP Service disconnected")
-    }
-}
