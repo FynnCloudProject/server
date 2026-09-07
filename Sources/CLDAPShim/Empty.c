29082f5101fd893798324732978cc575e6822f3c
Thilo <thilo0123@gmail.com>
2026-09-07 12:54:19 +0200

    Add ldap shim

---
+++ Sources/CLDAPShim/Empty.c
@@ -0,0 +1,2 @@
+// Intentionally empty. This C target exposes <ldap.h> and <lber.h> to Swift
+// (see include/Shim.h) for the native LDAPClient implementation.
---
+++ Sources/CLDAPShim/include/Shim.h
@@ -0,0 +1,8 @@
+#ifndef FYNNCLOUD_CLDAP_SHIM_H
+#define FYNNCLOUD_CLDAP_SHIM_H
+
+#include <lber.h>
+#include <ldap.h>
+
+#endif /* FYNNCLOUD_CLDAP_SHIM_H */
+
